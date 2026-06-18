d1806e12250d2b07751412eb38aaad0e3b13655d
dhruvjaiswall <167645473+dhruvjaiswall@users.noreply.github.com>
2026-06-18 21:23:44 +0530

    Add files via upload

---
+++ revision.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+// int x = 2;
+// float y = 5;
+// int exp1 = (x * y / x); 
+// int exp2 = (x * (y / x)); 
+// cout << exp1 << ",";
+// cout << exp2 << "\n";
+
+                      //leap year or not
+//     cout<<"enter the year :"<<endl;
+//     int year;
+//     cin>>year;
+//     if( year%4==0 && year%100==0 || year%400==0){
+//         cout<<"this is leap year";
+//    }
+//    else{
+//     cout<<"this is not a leap year";
+//    }
+                                 //armstrongg or not
+//     int arm,lstd;
+//     int t=0;
+
+//     cout<<"enter the number you wanna check if its armstrong or not";
+// cin >>arm;
+// int original=arm;
+// for(;arm>0;arm/=10){
+//     lstd=arm%10;
+//     t=(lstd*lstd*lstd+t);
+// }
+// if(t==original){
+//     cout <<"this is armstrong number";
+// }
+//     else{
+//         cout<<"this is not armstrong number";
+// }
+    //                               //factorial of no
+    // int no;
+    // cin>>no;
+    // int fact=1;
+    // if(no==0){
+    //     cout<<"1";
+    // }
+    // else{
+    // for(int i=1;i<=no;i++){
+    //     fact*=i;
+
+    // }cout<<fact;
+    // }
+                            //prime 
+
+    // int n,x;
+    
+    // bool isbool=true;
+    // cin >> n;
+    // for(x=2;x<=n-1;x++){
+    //     if(n%x==0){
+    //         isbool=false;
+    //         break;
+    //     }
+    // }
+    // if(isbool){
+    //     cout << "it is prime";
+    // }
+    // else{
+    //     cout << "it is not prime";
+    // }
+    return 0;
+}
